dcd48ef9d40a4c13686f33aade69b32c881ab3e1
Denilson Sá <denilsonsa@gmail.com>
2015-12-07 15:55:42 -0200

    Improving add_timestamp_to_output.py

--- add_timestamp_to_output.py
+++ add_timestamp_to_output.py
@@ -3,6 +3,7 @@
 import argparse
 import codecs
 import datetime
+import re
 import os
 import subprocess
 import sys
@@ -50,10 +51,73 @@ def parse_arguments():
 
 
 def unicode_escape(s):
-    if isinstance(s, bytes):
-        #s = s.decode('utf-8', 'replace')
-        s = s.decode('utf-8', 'strict')
+    '''Receives a byte-string, returns a byte-string.
+
+    Besides escaping, auto-adds a trailing newline.
+    (Why? Because it is convenient in the main code.)
+    '''
     return codecs.encode(s, 'unicode_escape') + b'\n'
+
+
+def escape_control_characters(s):
+    r'''Receives a byte-string, returns a byte-string.
+
+    Besides escaping, auto-adds a trailing newline.
+    (Why? Because it is convenient in the main code.)
+
+    >>> escape_control_characters(b'')
+    b'\n'
+    >>> escape_control_characters(b'foo')
+    b'foo\n'
+    >>> escape_control_characters(b'a\x00b\x01c\rd\te\nf\\g\x19h i\x80j\xffk')
+    b'a\\x00b\\x01c\\rd\\te\\nf\\\\g\\x19h i\x80j\xffk\n'
+    '''
+
+    def func(match):
+        c = match.group(0)
+        if c == b'\t':
+            return b'\\t'
+        elif c == b'\r':
+            return b'\\r'
+        elif c == b'\n':
+            return b'\\n'
+        elif c == b'\\':
+            return b'\\\\'
+        else:
+            return '\\x{0:02x}'.format(ord(c)).encode('ascii')
+
+    return re.sub(b'[\\x00-\\x1f\\\\]', func, s) + b'\n'
+
+
+def split_bytes_in_lines(blob):
+    r'''Splits the input (of type bytes) into a list of byte-strings, keeping
+    the newline separator. Avoids a trailing empty element if the last byte is
+    a newline.
+
+    >>> split_bytes_in_lines(b'')
+    []
+    >>> split_bytes_in_lines(b'\n')
+    [b'\n']
+    >>> split_bytes_in_lines(b'\n\n')
+    [b'\n', b'\n']
+    >>> split_bytes_in_lines(b'foo')
+    [b'foo']
+    >>> split_bytes_in_lines(b'foo\n')
+    [b'foo\n']
+    >>> split_bytes_in_lines(b'foo\nbar')
+    [b'foo\n', b'bar']
+    >>> split_bytes_in_lines(b'foo\nbar\n')
+    [b'foo\n', b'bar\n']
+    >>> split_bytes_in_lines(b'foo\nbar\nstuff')
+    [b'foo\n', b'bar\n', b'stuff']
+    '''
+    lines = blob.split(b'\n')
+    # Putting back the separator.
+    lines = [line + b'\n' for line in lines[:-1]] + lines[-1:]
+    # Removing the trailing empty string.
+    if lines[-1] == b'':
+        lines = lines[:-1]
+    return lines
 
 
 def main():
@@ -66,8 +130,12 @@ def main():
     # However, Python's stdin is buffered, so we need to reopen the fd.
     # Alternatively, Python's "-u" command-line option makes stdin unbuffered
     # in some Python versions (but not all).
+
+    # Input stream.
     in_file = os.fdopen(sys.stdin.fileno(), 'rb', buffering=0)
+    # Pass-through stream (i.e. output is the same as the raw input).
     out_raw = None
+    # Time-stamped output stream.
     out_ts = sys.stdout.buffer
 
     if OPTIONS.output:
@@ -80,7 +148,8 @@ def main():
     escape = lambda x: x
 
     if OPTIONS.escape:
-        escape = unicode_escape
+        # escape = unicode_escape
+        escape = escape_control_characters
 
     # Timestamp digits.
     timestamp_format = '{0:.' + str(OPTIONS.digits) + 'F}s\t'
@@ -92,28 +161,37 @@ def main():
     previous_datetime = start_datetime
 
     while True:
-        # Reading at most 64KB of data.
+        # Reading at most 64KB of data on each iteration.
         raw_data = in_file.read(64 * 1024)
         if len(raw_data) == 0:
             break
 
+        # Calculating the elapsed time.
         current_datetime = datetime.datetime.utcnow()
-
         if start_datetime is None:
             # This happens only once.
             start_datetime = current_datetime
-            previous_datetime = start_datetime
+            previous_datetime = current_datetime
             seconds = 0.0
         else:
+            # This gets executed on every iteration after the first one.
             if OPTIONS.absolute:
                 delta = current_datetime - start_datetime
             else:
                 delta = current_datetime - previous_datetime
             seconds = delta.total_seconds()
+            previous_datetime = current_datetime
 
-        previous_datetime = current_datetime
-
-        output = timestamp_format.format(seconds).encode('ascii') + escape(raw_data)
+        # Building the output for this input blob.
+        raw_lines = split_bytes_in_lines(raw_data)
+        output = b''
+        for raw_line in raw_lines:
+            timestamp_bytes = timestamp_format.format(seconds).encode('ascii')
+            output += timestamp_bytes + escape(raw_line)
+            if not OPTIONS.absolute:
+                # If there are multiple lines in this blob, all lines after
+                # the first one have a delta-time of zero.
+                seconds = 0
 
         if out_ts:
             out_ts.write(output)
